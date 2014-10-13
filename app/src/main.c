e23d9b63b6182e0c2ea5f252521745406d6823ca
fflasch <fflasch@debianfrqdevvm>
2014-10-13 15:32:31 +0200

    Added delay function, corrected LED GPIO

--- app/src/main.c
+++ app/src/main.c
@@ -2,6 +2,11 @@
 #include <stm32f10x_conf.h>
 #include <stm32_configuration.h>
 
+void Delay(__IO uint32_t nCount)
+{
+  for(; nCount != 0; nCount--);
+}
+
 int main()
 {
 	RCC_Configuration();
@@ -10,8 +15,10 @@ int main()
 
 	while(1)
 	{
-		GPIO_SetBits(GPIOC, GPIO_Pin_12);
-		GPIO_ResetBits(GPIOC, GPIO_Pin_12);
+		GPIO_SetBits(GPIOC, GPIO_Pin_8);
+		Delay(10000);
+		GPIO_ResetBits(GPIOC, GPIO_Pin_8);
+		Delay(10000);
 	}
 }
 
--- app/src/stm32_configuration.c
+++ app/src/stm32_configuration.c
@@ -39,7 +39,7 @@ void GPIO_Configuration(void)
 {
 	GPIO_InitTypeDef GPIO_InitStructure;
 
-	GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_12;
+	GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_8;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(GPIOC, &GPIO_InitStructure);
